7a1a69058505bd92899e7c051fa0f7dc3f9235cf
BHXiao <3425297274@qq.com>
2021-08-26 17:21:48 +0800

    新增一些脚本

--- 办公自动化/e.py
+++ 办公自动化/e.py
@@ -58,6 +58,7 @@
 #     '国内用户服务部': '营销系统/用服部',
 #     '国内销售部': '营销系统/国内销售部',
 #     '国际销售部': '营销系统/International sales department',
+#     'IVD国际销售部': '营销系统/International sales department',
 #     '技术支持服务部': '营销系统/用服部',
 #     '用户服务部': '营销系统/用服部',
 #     '国际用户服务部': '营销系统/国际用户服务部',
@@ -89,6 +90,10 @@
 #     '生命科学产品管理部': '生命科学',
 #     '采购商务部': '生产及供应链系统/采购部/采购商务部',
 #     '试剂生产部': '生产及供应链系统/试剂制造部/试剂生产部',
+#     '默认部门': '默认部门',
+#     '仪器工程部': '生产及供应链系统/仪器制造部/仪器工程部',
+#     '研发管理部': '研发部/研发管理部',
+#     # '': '',
 #     # '': '',
 #     # '': '',
 #     # '': '',
@@ -202,8 +207,129 @@
 # print(mydoubler(11))
 
 ######################################################################################
+# # 控制浏览调整邮箱分组
+# import pyperclip
+# import pyautogui
+# import easygui
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from openpyxl import load_workbook
+#
+# open_file = easygui.fileopenbox(title='选择原始信息文件', default=r'D:\Users\5006554\Desktop\*.xlsx')
+#
+# wb = load_workbook(open_file, read_only=True)
+# ws = wb.active
+# driver = webdriver.Chrome()
+# # time.sleep(5)
+# driver.get('https://qiye.163.com/login/')
+# driver.find_element_by_id("switchAdminCtrl").click()
+# driver.find_element_by_id("adminname").send_keys('ben.xiao@medcaptain.com')
+# driver.find_element_by_id("adminpwd").send_keys('nihao,8090')
+# driver.find_element_by_xpath('//*[@id="loginBlock"]/div[2]/form[2]/div[5]/button').click()
+# # print(driver.window_handles)
+# input()
+# driver.find_element_by_xpath('/html/body/div[5]/div/div/div/button').click()
+# driver.find_element_by_xpath('//*[@id="headTabs"]/div[1]/ul/li[3]/a/tab-heading/span').click()
+# time.sleep(1)
+# for i in range(1, ws.max_row):
+#     text = str(ws.cell(row=i + 1, column=1).value)
+#     text1 = str(ws.cell(row=i + 1, column=5).value)
+#     driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[2]/div/div[1]/form/div[2]/div[2]/div/input').clear()
+#     driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[2]/div/div[1]/form/div[2]/div[2]/div/input').send_keys(text)
+#     driver.find_element_by_xpath('/html/body/div[3]/div/div').click()
+#     driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[2]/div/div[2]/div[1]/table/tbody[2]/tr/td[1]/input').click()
+#     driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[2]/div/div[1]/form/div[2]/div[1]/div[2]/button').click()
+#     time.sleep(0.5)
+#     driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[2]/div/div[1]/div[2]/div[1]/div[1]/input').send_keys(text1)
+#     driver.find_element_by_xpath('//*[@id="searchUnitTreeContainer"]/ul/li/label/span[2]').click()
+#     driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[2]/div/button[1]').click()
+#     driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[1]').click()
+#     driver.find_element_by_xpath('//*[@id="searchUnitTreeContainer"]/ul/li/label/span[2]').click()
+#     driver.find_element_by_xpath('//*[@id="searchUnitTreeContainer"]/ul/li/label/span[2]').click()
+#     time.sleep(4)
+# time.sleep(4)
+# driver.quit()
+###########################################################################################################################
+# import pyperclip 复制后 pyautogui 找不到窗口句柄问题
+# import win32gui
+# import win32api
+# import pyautogui
+#
+# # from pymouse import PyMouse
+# hwnd_title = {}
+#
+#
+# def get_all_hwnd(hwnd, mouse):
+#     if (win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd)):
+#         hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
+#
+#
+# win32gui.EnumWindows(get_all_hwnd, 0)
+#
+# # m = PyMouse()
+#
+# for h, t in hwnd_title.items():
+#     if t:
+#         print(h, t)
+#         if 'Mozilla Firefox' in t:
+#             win32gui.GetWindow(h)
+#             # left, top, right, bottom = win32gui.GetWindowRect(h)
+#             # print(left, top, right, bottom)
+#             # pyautogui.click(right - 206, bottom - 31)
 
 
+######################################################################################################################
+# 验证让面猜想
+import win32gui
+import time
+from openpyxl import load_workbook
+import pyperclip
+import pyautogui
+import easygui
+import pyHook
 
+
+# def get_all_hwnd(hwnd, mouse):
+#     if (win32gui.IsWindow(hwnd) and win32gui.IsWindowEnabled(hwnd) and win32gui.IsWindowVisible(hwnd)):
+#         hwnd_title.update({hwnd: win32gui.GetWindowText(hwnd)})
+
+hwnd_title = {}
+file = easygui.fileopenbox(title='选择原始信息文件', default=r'D:\Users\5006554\Desktop\*.xlsx')
+wb = load_workbook(file)
+ws = wb.active   #  没有括号
+name = []
+# 创建一个“钩子”管理对象
+hm = pyHook.HookManager()
+for i in range(1, ws.max_row):
+    name += ws.cell(row=i+1, column=1).value,
+    print(name)
+for i in name:
+
+
+        # win32gui.EnumWindows(get_all_hwnd, 0)  # 生成一个窗口句柄的字典，存放所有打开的窗口
+        # for h, t in hwnd_title.items():
+        #     if t:
+        #         print(h, t)
+        #         if 'Mozilla Firefox' in t:
+        #             # win32gui.GetWindow(int(h, base=10), t)
+        #             # 窗体前端显示
+        #             time.sleep(20)
+        #
+        #             win32gui.SetForegroundWindow(h)
+        #             time.sleep(20)
+        #             break
+
+        print(i)
+        pyautogui.click(1630, 316)
+        pyperclip.copy(i)  # 这里切换了窗口
+        print(i)
+
+        pyautogui.hotkey('ctrl', 'a')  # 这里只是操作键盘，所以不报错
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press('enter')
+        while True:
+
+        pyautogui.click(1000, 700)
 
 
